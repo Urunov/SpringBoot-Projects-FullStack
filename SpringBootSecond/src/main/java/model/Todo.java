4362dcbf90df4eef22bbaeaab279d3ff92c53e32
Urunov <hamdamboy.urunov@gmail.com>
2020-02-27 18:00:17 +0900

    Spring Boot MySQL Security todo

--- SpringBootSecond/src/main/java/model/Todo.java
+++ SpringBootSecond/src/main/java/model/Todo.java
@@ -61,3 +61,4 @@ public class Todo {
 
 // https://www.javaguides.net/2018/09/mini-todo-management-project-using-spring-boot-springmvc-springsecurity-jsp-hibernate-mysql.html
 
+// https://www.javaguides.net/2018/02/liskov-substitution-principle.html
