25f315cb2df4befe56cfe960b553fa12a2409e6e
apple <hamdamboy.urunov@gmail.com>
2020-03-29 00:10:36 +0900

    Spring Boot MVC Web Postgresql

--- Part-4 Spring Boot REST API/SpringPostgreslq/src/main/java/spring/database/DatabaseApplication.java
+++ Part-4 Spring Boot REST API/SpringPostgreslq/src/main/java/spring/database/DatabaseApplication.java
@@ -2,8 +2,10 @@ package spring.database;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DatabaseApplication {
 
     public static void main(String[] args) {
