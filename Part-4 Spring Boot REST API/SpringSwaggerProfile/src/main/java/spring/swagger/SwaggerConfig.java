4b57b93624545b9ea5acef9f6ead73521704c026
apple <hamdamboy.urunov@gmail.com>
2020-04-04 22:36:35 +0900

    Spring Boot Swagger2 Maven

---
+++ Part-4 Spring Boot REST API/SpringSwaggerProfile/src/main/java/spring/swagger/SwaggerConfig.java
@@ -0,0 +1,48 @@
+package spring.swagger;
+
+import org.springframework.context.annotation.Bean;
+import static com.google.common.base.Predicates.or;
+import static springfox.documentation.builders.PathSelectors.regex;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import com.google.common.base.Predicate;
+
+/**
+ * @Author: apple
+ * @created on 04/04/2020
+ * @Project is SpringSwaggerProfile
+ */
+
+@Configuration
+@EnableSwagger2
+@Profile("swagger-enabled-for-qa")
+
+public class SwaggerConfig {
+
+    @Bean
+    public Docket postsApi() {
+        return new Docket(DocumentationType.SWAGGER_2).groupName("public-api")
+                .apiInfo(apiInfo()).select().paths(postPaths()).build();
+    }
+
+    private Predicate postPaths() {
+        return or(regex("/api/posts.*"), regex("/api/javainuse.*"));
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("JavaInUse API")
+                .description("JavaInUse API reference for developers")
+                .termsOfServiceUrl("http://javainuse.com")
+                .contact("javainuse@gmail.com").license("JavaInUse License")
+                .licenseUrl("javainuse@gmail.com").version("1.0").build();
+    }
+
+}
