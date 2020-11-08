9cc4ea2630896e606323a82458a5a91bb95bba05
Hamdamboy <hamdamboy.urunov@gmail.com>
2020-11-08 10:30:22 +0900

    FullStack Spring Back Shopping Mall

--- Part-9.SpringBoot-React-Projects/Project-2.SpringBoot-React-ShoppingMall/fullstack/backend/src/main/java/com/urunov/model/Offer.java
+++ Part-9.SpringBoot-React-Projects/Project-2.SpringBoot-React-ShoppingMall/fullstack/backend/src/main/java/com/urunov/model/Offer.java
@@ -1,10 +1,37 @@
 package com.urunov.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+
 /**
  * Created by:
  * User: hamdamboy
  * Project: IntelliJ IDEA
  * Github: @urunov
  */
+
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
 public class Offer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private byte[] image;
+
+    @Transient
+    private String imageUrl;
+
+    @Lob
+    private String description;
+
+
 }
