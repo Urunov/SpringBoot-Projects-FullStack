17d67307d6a1a00a72d1af91f95c074896fdf798
Hamdamboy <hamdamboy.urunov@gmail.com>
2021-02-12 22:21:22 +0900

    MongoDB

--- Part-9.SpringBoot-React-Projects/Project-8.SpringBoot-GitApi-CodeChecking/src/main/java/com/urunov/pairing/repository/GitCommitRepo.java
+++ Part-9.SpringBoot-React-Projects/Project-8.SpringBoot-GitApi-CodeChecking/src/main/java/com/urunov/pairing/repository/GitCommitRepo.java
@@ -2,6 +2,8 @@ package com.urunov.pairing.repository;
 
 import com.urunov.pairing.data.GitCommit;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GitCommitRepo extends MongoRepository<GitCommit, Integer> {
 }
