93c1488911a2a2dc71cbfbfe5ebda3383bea8d37
Davr bank <backend@davrbank.uz>
2022-08-12 18:10:54 +0500

    Basic Auth

---
+++ Advanced-SpringSecure/1. simple-secure/secure-start/src/main/java/com/secure/sytem/securestart/controller/StudentManagementController.java
@@ -0,0 +1,47 @@
+package com.secure.sytem.securestart.controller;
+
+import com.secure.sytem.securestart.entity.Student;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @project: secure-start
+ * @Date: 12.08.2022
+ * @author: H_Urunov
+ **/
+@RestController
+@RequestMapping("/management/api/v1/students")
+public class StudentManagementController {
+    //
+    private static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1, "James Bond"),
+            new Student(2, "Lary Gaga"),
+            new Student(3, "Faktor2"),
+            new Student(4, "Anna "),
+            new Student(5, "Anna German ")
+    );
+
+    @GetMapping
+    public List<Student> getAllStudents(){
+        return STUDENTS;
+    }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student){
+        System.out.println("registerNewStudent");
+        System.out.println(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable() Integer studentId){
+        System.out.println("deleteStudent");
+        System.out.println(studentId);
+    }
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(@PathVariable("studentId") Integer studentId, @RequestBody Student student){
+        System.out.println("Update student INFO.");
+        System.out.println(String.format("%s %s", studentId, student));
+    }
+}
---
+++ Advanced-SpringSecure/1. simple-secure/secure-start/src/main/java/com/secure/sytem/securestart/security/PasswordConfig.java
@@ -0,0 +1,20 @@
+package com.secure.sytem.securestart.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @project: secure-start
+ * @Date: 12.08.2022
+ * @author: H_Urunov
+ **/
+@Configuration
+public class PasswordConfig {
+    //
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
+    }
+}
